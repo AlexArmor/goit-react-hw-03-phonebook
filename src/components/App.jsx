8781ef4edef37e8874beee5d35fe0d1a17e21d1c
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-22 18:15:11 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,26 @@ import { Section } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const data = localStorage.getItem('contacts');
+
+    if (data) {
+      const parsedData = JSON.parse(data);
+      this.setState({
+        contacts: parsedData,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   onFormSubmit = text => {
     const isAtList = this.state.contacts.find(
